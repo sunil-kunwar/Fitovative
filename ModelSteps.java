410b8d917e5c3961e6328fdc7fb1192b43b9f9b7
Sunil Kunwar <50283192+sunil-kunwar@users.noreply.github.com>
2019-05-29 17:01:30 +1000

    Create ModelSteps.java

---
+++ ModelSteps.java
@@ -0,0 +1,21 @@
+package com.example.fitnessapp;
+
+public class ModelSteps {
+    public String getSteps() {
+        return steps;
+    }
+
+    public void setSteps(String steps) {
+        this.steps = steps;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    String steps,time;
+}
